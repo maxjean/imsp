6a44c39d3f316c9b0e10db6df2deba4b72585d59
Max <maxclifordjean@gmail.Com>
2015-04-30 16:14:59 +0200

    navigation bar with link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,14 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+=begin
   before_filter :set_access_control_headers
-
-  def set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = "*"
-    headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
-  end
+=end
 
   def home
   end
